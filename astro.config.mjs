a0ed6f67e152f011a46d24b8ffd6611242faa0b3
Gabriel-ISS <gabrielsykora693@gmail.com>
2025-04-25 19:23:48 -0300

    Fix production fetch 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/echoes-of-ancestral-wars/' : '/',
+  base: '/echoes-of-ancestral-wars',
 });
--- src/utils/Engine/constants.ts
+++ src/utils/Engine/constants.ts
@@ -13,5 +13,5 @@ export const Config = {
 };
 
 // todo esto porque en la build temporalmente es indefinido y se rompe
-export const BASE_PATH = (import.meta.env.BASE_URL || '/').replace(/\/$/, '') + '/';
+export const BASE_PATH = import.meta.env.BASE_URL + '/';
 console.log('BASE_PATH', BASE_PATH);
