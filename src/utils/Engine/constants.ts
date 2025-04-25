0937cff23ba63a06e2265be4ce9000c3a0e1512c
Gabriel-ISS <gabrielsykora693@gmail.com>
2025-04-25 20:48:16 -0300

    Solución manual

--- src/utils/Engine/constants.ts
+++ src/utils/Engine/constants.ts
@@ -14,4 +14,3 @@ export const Config = {
 
 // todo esto porque en la build temporalmente es indefinido y se rompe
 export const BASE_PATH = import.meta.env.BASE_URL + '/';
-console.log('BASE_PATH', BASE_PATH);
